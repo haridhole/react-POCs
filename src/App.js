0b1883a2273c16c099eed57c858095f5c280ea05
haridhole <hari.dhole@circles.asia>
2021-10-05 13:03:10 +0530

    cloudinary POC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import Axios from "axios";
+import { Image } from "cloudinary-react";
 import './App.css';
 
 function App() {
+
+  const [imageSelected, setImageSelected] = useState("");
+
+  const uploadImage = () =>{
+    const formData = new FormData();
+    formData.append("file", imageSelected);
+    formData.append("upload_preset", "react_demo");
+
+    Axios.post("https://api.cloudinary.com/v1_1/dcayfmk9r/image/upload", formData)
+      .then(response=>console.log(response));
+
+    console.log("file", imageSelected);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={(e) => setImageSelected(e.target.files[0])}/>
+      <button onClick={uploadImage}>Upload Image</button>
+      <Image style={{width:200}} cloudName="dcayfmk9r" publicId="https://res.cloudinary.com/dcayfmk9r/image/upload/v1633418769/gf6px7zv92jmpljd6nsc.png"/>
     </div>
   );
 }
